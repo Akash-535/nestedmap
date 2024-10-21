be9a56af69bcfc22ce4a8f9443cafbc374840861
akash-535 <akashbish9e@gmail.com>
2024-10-21 16:37:40 +0530

    all done

--- src/components/common/Helper.jsx
+++ src/components/common/Helper.jsx
@@ -40,3 +40,45 @@ export const CARD_DATA = [
         }
     ]
 
+    export const CARD_DATA_TWO = [
+        {
+            heading: 'Card Two',
+            post: 'Doctor',
+            description: 'Lorem ipsum dolor sit amet consectetur. Velit pulvinar libero porttitor quam sed aliquet. Sodales vulputate elementum arcu donec. Feugiat nibh ullamcorper egestas lectus risus ornare ullamcorper fames. Cursus libero nec facilisis risus malesuada arcu cum. Gravida massa scelerisque sociis nulla diam nisl ligula.bh ',
+            content :[
+                {
+                   item:'Doctor' 
+                },
+                {
+                    item:'about-doctor'
+                }
+            ]
+        },
+        {
+            heading: 'Card Two',
+            post: 'Police',
+            description: 'Lorem ipsum dolor sit amet consectetur. Velit pulvinar libero porttitor quam sed aliquet. Sodales vulputate elementum arcu donec. Feugiat nibh ullamcorper egestas lectus risus ornare ullamcorper fames. Cursus libero nec facilisis risus malesuada arcu cum. Gravida massa scelerisque sociis nulla diam nisl ligula.bh ',
+            content :[
+                {
+                  item:'police'
+                },
+                {
+                    item:'about-police'
+                  },
+            ]
+            
+        },
+        {
+            heading: 'Card Two',
+            post: 'Advocate',
+            description: 'Lorem ipsum dolor sit amet consectetur. Velit pulvinar libero porttitor quam sed aliquet. Sodales vulputate elementum arcu donec. Feugiat nibh ullamcorper egestas lectus risus ornare ullamcorper fames. Cursus libero nec facilisis risus malesuada arcu cum. Gravida massa scelerisque sociis nulla diam nisl ligula.bh ',
+            content :[
+                {
+                   item:'Doctor',},
+                   {
+                    item:'about-Advocate'
+                }
+            ]
+        }
+    ]
+
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import { LoreIcon } from '../common/Icons'
+import { CARD_DATA_TWO } from '../common/Helper'
 
 const Home = () => {
   return (
-    <div>
-      <LoreIcon/>
-      <p>home</p>
+    <div className='flex gap-5 min-h-screen justify-center items-center px-3'>
+      {CARD_DATA_TWO.map((obj , index) => (
+          <div key={index} className='bg-gray-500 p-5 gap-5 flex flex-col hover:bg-white hover:shadow-2xl hover:border hover:border-red-500 duration-300 ease-linear group'>
+          <h2 className='text-2xl flex items-center text-white group-hover:text-black'><LoreIcon/>{obj.heading}
+                    </h2>
+           <p className='text-lg text-white group-hover:text-black'>{obj.post}</p>
+           <p className='text-sm text-white group-hover:text-black'>{obj.description}</p>    
+           {obj.content.map((val , i) => (
+         <a href='#' key={i} className='text-white group-hover:text-black'>
+           {val.item}
+         </a>
+       ))}      
+                   </div>
+      ))}
     </div>
   )
 }
